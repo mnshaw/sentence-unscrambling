ec997ec7d24002932c9535b6ed218b1fd50e4d2a
mnshaw <mnshaw@andrew.cmu.edu>
2018-04-27 17:46:55 -0400

    Add priming sentences

--- example/sentence.js
+++ example/sentence.js
@@ -1,5 +1,5 @@
 var j = 0;
-var sentences = [["hi", "bye"], ["woo", "boo"]];
+var sentences = [["cool", "is", "Today", "great"], ["The", "warm", "is", "collected", "summer"], ["healthy", "Apples", "are", "tranquil"], ["serene", "is", "Coffee", "good"], ["The", "relax", "flew", "dove", "away"], ["blooming", "The", "meditating", "flowers", "are"], ["rises", "sun", "peace", "The", "east", "in"], ["trees", "The", "soothing", "are", "tall"], ["pollen", "Bees", "collect", "happy"], ["smelt", "air", "flowers", "The", "smooth", "like"], ["away", "The", "glassy", "fish", "swam"], ["park", "in", "the", "sunny", "harmony", "A", "day"], ["harmony", "The", "played", "a", "violin", "rest", "soft"], ["near", "beach", "soft", "A", "house", "the"], ["breeze", "Tomorrow", "great", "be", "will"], ["swayed", "breeze", "in", "waves", "the", "swing", "The"], ["load", "of", "beach", "A", "laundry", "fresh"], ["clean", "red", "Roses", "are"], ["shore", "breathe", "The", "tide", "to", "came"]];
 var done = false;
 
 function submit() {
